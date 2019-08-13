27b0d7928e7564185cab905c7484f8cb49ca69cd
Jiecheng Chen <chjchjason@gmail.com>
2019-08-12 22:11:04 -0700

    1-example project

---
+++ 1-spring_Boot_Example/src/main/java/com/jc/springboot/Applicaiton.java
@@ -0,0 +1,12 @@
+package com.jc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //springboot的引导类
+public class Applicaiton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Applicaiton.class, args);
+    }
+}
